3d1058daf3312adbbddcd08d934660a65c042885
wasym.atieh <wasym.atieh@upcnet.es>
2022-05-27 14:04:23 +0200

    Solving certificate problem (UNSOLVED)

--- puppet/puppet_list-nodes.py
+++ puppet/puppet_list-nodes.py
@@ -5,11 +5,11 @@ import puppet
 
 # Puppet Master object instantiation
 # (Load Balancer Puppet Master)
-p = puppet.Puppet(host='puppet.pre.upc.edu',
+p = puppet.Puppet(host='puppet-m1-pre.upc.edu',
                   port=8140,
-                  key_file=None,
-                  cert_file=None,
-                  ssl_verify=False,
+                  key_file='ssl/kali.upc.edu.key',
+                  cert_file='ssl/kali.upc.edu.pem',
+                  ssl_verify=True,
                   cache_enabled=True,
                   cache_file='pypuppet_cache',
                   cache_backend='sqlite',
